a80be3a49e7c4edd0a9653f74a909820132ee2de
Whitelotus07 <josephkazumi7@gmail.com>
2024-11-09 10:37:49 +0100

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   // Replace with your Firebase config
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyBpDX5PJ0AdWdbFb8JeUfN04lyag8NFJwU",
   authDomain: "pulse-tap.firebaseapp.com",
   projectId: "pulse-tap",
   storageBucket: "pulse-tap.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  messagingSenderId: "362721898715",
+  appId: "1:362721898715:web:0b4fee0f30706edb879cfe",
+  measurementId: "G-5S6R1HKJD5"
 };
 
 const app = initializeApp(firebaseConfig);
