7f297a0e915ee21a1e8f29439a4e7aa39fc474d3
Whitelotus07 <josephkazumi7@gmail.com>
2024-11-08 18:39:25 -0800

    Update TonConnect.tsx

--- src/components/TonConnect.tsx
+++ src/components/TonConnect.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { TonConnectButton } from '@tonconnect/ui-react';
-import { useTonConnect } from '@tonconnect/ui-react';
 import toast from 'react-hot-toast';
 
 export default function TonConnect() {
-  const { connected } = useTonConnect();
+  const handleConnectionChange = (isConnected) => {
+    if (isConnected) {
+      toast.success('Wallet connected!');
+    } else {
+      toast.error('Wallet disconnected.');
+    }
+  };
 
   return (
     <div className="fixed top-4 right-4 z-50">
-      <TonConnectButton />
-      {!connected && (
-        <div className="mt-2 text-sm text-gray-400 text-right">
-          Connect wallet to unlock features
-        </div>
-      )}
+      <TonConnectButton onClick={() => handleConnectionChange(true)} />
+      {/* Display a message based on your connection state logic */}
+      <div className="mt-2 text-sm text-gray-400 text-right">
+        Connect wallet to unlock features
+      </div>
     </div>
   );
 }
