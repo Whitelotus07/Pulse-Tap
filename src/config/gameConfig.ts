6eb8b6e36e3029fa736c2084ed2a922588fbe662
Whitelotus07 <josephkazumi7@gmail.com>
2024-11-10 03:41:18 +0100

    Update gameConfig.ts

--- src/config/gameConfig.ts
+++ src/config/gameConfig.ts
@@ -2,13 +2,13 @@ export const LEVELS = [
   { id: 0, name: "Beginner", requiredTaps: 0, bonus: 0, icon: "🌱", baseIncome: 1, skipPrice: 1 },
   { id: 1, name: "Novice", requiredTaps: 1000, bonus: 1000, icon: "⚡", baseIncome: 10, skipPrice: 7 },
   { id: 2, name: "Apprentice", requiredTaps: 10000, bonus: 10000, icon: "🔥", baseIncome: 100, skipPrice: 10 },
-  { id: 3, name: "Expert", requiredTaps: 100000, bonus: 100000, icon: "💫", baseIncome: 1000},
-  { id: 4, name: "Master", requiredTaps: 1000000, bonus: 1000000, icon: "🌟", baseIncome: 10000},
-  { id: 5, name: "Grand Master", requiredTaps: 100000000, bonus: 100000000, icon: "👑", baseIncome: 100000},
-  { id: 6, name: "Legend", requiredTaps: 100000000, bonus: 100000000, icon: "🏆", baseIncome: 1000000},
-  { id: 7, name: "Mythical", requiredTaps: 10000000000, bonus: 10000000000, icon: "🌈", baseIncome: 10000000},
-  { id: 8, name: "Divine", requiredTaps: 50000000000, bonus: 50000000000, icon: "⭐", baseIncome: 100000000},
-  { id: 9, name: "Immortal", requiredTaps: 90000000000, bonus: 90000000000, icon: "🌌", baseIncome: 1000000000}
+  { id: 3, name: "Expert", requiredTaps: 100000, bonus: 100000, icon: "💫", baseIncome: 1000, skipPrice: 15 },
+  { id: 4, name: "Master", requiredTaps: 1000000, bonus: 1000000, icon: "🌟", baseIncome: 10000 },
+  { id: 5, name: "Grand Master", requiredTaps: 100000000, bonus: 100000000, icon: "👑", baseIncome: 100000 },
+  { id: 6, name: "Legend", requiredTaps: 100000000, bonus: 100000000, icon: "🏆", baseIncome: 1000000 },
+  { id: 7, name: "Mythical", requiredTaps: 10000000000, bonus: 10000000000, icon: "🌈", baseIncome: 10000000 },
+  { id: 8, name: "Divine", requiredTaps: 50000000000, bonus: 50000000000, icon: "⭐", baseIncome: 100000000 },
+  { id: 9, name: "Immortal", requiredTaps: 90000000000, bonus: 90000000000, icon: "🌌", baseIncome: 1000000000 }
 ];
 
 export const TON_WALLET = "UQC7JxkjGCWm99IUZGknnU_ctGNkngboRyfalkRPPMV-34M0";
