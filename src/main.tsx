3a870382d0c987733b18af7c43f224cc4e0db470
Whitelotus07 <josephkazumi7@gmail.com>
2024-11-11 19:56:09 -0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+// src/main.tsx
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { WalletProvider } from './context/WalletContext'; // Import the WalletProvider
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <WalletProvider> {/* Wrap App with WalletProvider */}
+      <App />
+    </WalletProvider>
   </StrictMode>
 );
