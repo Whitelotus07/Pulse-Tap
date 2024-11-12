ea4fd7a19c61985bb27150256222a959e01f0e5e
Whitelotus07 <josephkazumi7@gmail.com>
2024-11-11 17:23:08 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-// src/App.tsx
-
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
@@ -11,6 +9,7 @@ import Shop from './pages/Shop';
 import Leaderboard from './pages/Leaderboard';
 import Layout from './components/Layout';
 import ConnectWallet from './pages/ConnectWallet'; // Import the new ConnectWallet page
+import Levels from './pages/Levels'; // Import the new Levels page
 
 function App() {
   return (
@@ -22,6 +21,7 @@ function App() {
           <Route element={<Layout />}>
             <Route path="/connect-wallet" element={<ConnectWallet />} /> {/* New route for Connect Wallet */}
             <Route path="/game" element={<Game />} />
+            <Route path="/levels" element={<Levels />} /> {/* New route for Levels */}
             <Route path="/stats" element={<Stats />} />
             <Route path="/shop" element={<Shop />} />
             <Route path="/leaderboard" element={<Leaderboard />} />
